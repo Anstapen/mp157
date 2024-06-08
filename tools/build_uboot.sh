0253b54cf9d363ea305a8817451e9fcd612eb9d9
Anton Stapenhorst <anton.stapen@gmail.com>
2024-06-08 11:25:43 +0200

    Added custom defconfig

--- tools/build_uboot.sh
+++ tools/build_uboot.sh
@@ -4,9 +4,18 @@ DT="stm32mp157f-dk2"
 
 echo Building Uboot...
 
-if test -d third_party/uboot; then
-    echo "Found U-Boot Directory, continuing..."
+if ! test -d third_party/uboot; then
+    echo "Could not find uboot directory..."
 fi
+
+if ! test -d config; then
+    echo "Could not find config directory..."
+fi
+
+DEFCONFIG_FILE=custom_defconfig
+
+# just a hotfix for now, find a better solution...
+cp config/$DEFCONFIG_FILE third_party/uboot/configs
 
 rm -rf build
 mkdir build
@@ -16,12 +25,16 @@ echo Build Dir: $BUILD_DIR
 
 cd ../third_party/uboot
 
-echo "make O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- stm32mp15x_baremetal_defconfig"
+echo "make O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- $DEFCONFIG_FILE"
 echo "make -j8 O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- all"
 
 # First clean the U-Boot Directory
 
 make mrproper
 
-make O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- stm32mp15_basic_defconfig
+make O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- $DEFCONFIG_FILE
 make -j8 O=$BUILD_DIR DEVICE_TREE=$DT CROSS_COMPILE=arm-none-eabi- all
+
+
+echo Cleaning up...
+rm configs/$DEFCONFIG_FILE
